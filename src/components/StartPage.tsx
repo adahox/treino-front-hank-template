67d2a2ff3cb1a9703e32eb1f95a11df562177570
adao dias <adahox@gmail.com>
2022-07-07 15:10:36 -0300

    feature(nav): responsive mode startpage

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -9,23 +9,23 @@ export function StartPage() {
     return (
         <>
             <Nav />
-            {/* <section className='grid grid-cols-2 max-h-[985px] h-full items-center'>
-                <div className="flex-row justify-center">
-                    <h1 className='font-bold text-x-large text-primary-700'>
+            <section className='px-5 grid md:grid-cols-2 max-h-[985px] h-full lg:items-center items-start'>
+                <div className="flex-row justify-center pt-28 lg:pt-0  h-full lg:h-auto  lg:items-center">
+                    <h1 className='text-center md:text-left font-bold text-x-large text-primary-700'>
                         We create gaming breakthroughs.
                     </h1>
-                    <p className='font-medium text-primary-700'>
+                    <p className='text-center md:text-left font-medium text-primary-700'>
                         Hawk Industries is a company that inspires new ways to play games by combining innovative software with an easy to use interface.
                     </p>
-                    <div className="flex flex-row gap-4 mt-14">
+                    <div className="flex flex-col  md:flex-row gap-4 mt-14">
                         <button className='shadow-lg shadow-primary-700/40 border border-primary-900 font-bold text-primary-100 rounded bg-primary-900 px-[53px] py-4 hover:bg-primary-200 transition duration-150 hover:border-primary-200 hover:text-primary-700'>See Products</button>
                         <button className='shadow-lg shadow-primary-700/20 border border-primary-200 font-bold rounded text-primary-900 px-[53px] py-4 bg-primary-200'>Contact Us</button>
                     </div>
                 </div>
-                <div className='h-full w-full ml-6'>
+                <div className='hidden sm:h-full md:block w-full sm:ml-6'>
                     <img alt="soldado" src={getImageUrl()} />
                 </div>
-            </section> */}
+            </section>
         </>
     );
 }
